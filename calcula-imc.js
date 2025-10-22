76576a9dac69433bebda5af6f77892cf9d495d61
lettitu <leticia.pereira.araujo@escola.pr.gov.br>
2025-10-22 09:07:58 -0300

    Add files via upload

--- calcula-imc.js
+++ calcula-imc.js
@@ -14,33 +14,49 @@ var pacientes = document.querySelectorAll(".paciente");
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
+    if (!pesoEhValido) {
       console.log("Peso Invalido!");
       pesoEhValido = false;
       tdImc.textContent = "Peso Invalido!";
       paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.00) {
+    if (!alturaEhValida) {
       console.log("Altura Invalida!");
       alturaEhValida = false;
       tdImc.textContent = "Altura Invalida!";
       paciente.classList.add("paciente-invalido");
     }
 
-    if ( alturaEhValida && pesoEhValido) {
+    if (alturaEhValida && pesoEhValido) {
       var imc = calculaImc(peso, altura);
       tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso) {
+
+  if(peso >= 0 && peso <= 1000) {
+    return true;
+  } else {
+    return false;
   }
 }
-  
-function calculaImc(peso, altura){
-    var imc = 0;
-  
-    imc = peso / (altura * altura);
-  
-    return imc.toFixed(2);
+
+function validaAltura(altura) {
+  if(altura >= 0 && altura <= 3.0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+function calculaImc(peso, altura) {
+  var imc = 0;
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
 }
